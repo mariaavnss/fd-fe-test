cf36c8d1676550ae2b31fde4407b25b2d5edb3d0
MARIA VANESSA SALIM <mariavanessasalim@gmail.com>
2022-08-21 09:51:44 +0700

    Create section EditorsChoice

--- src/sections/EditorChoice.js
+++ src/sections/EditorChoice.js
@@ -1,16 +1,75 @@
 import React from "react";
+import styled from "styled-components";
 
 import SectionHeader from "../components/SectionHeader";
+import Cards from "../components/Cards";
+
+const CardWrapper = styled.div`
+  display: flex;
+  gap: 10px;
+`;
+
+const Container = styled.div`
+  padding: 0 150px;
+`
 
 function EditorChoice() {
-  return <React.Fragment>
-    <SectionHeader
-      title={"Editor's Choice"}
-      subtitle={"Curated with love"}
-      seeMore={false}
-    />
-
-  </React.Fragment>;
+  return (
+    <Container>
+      <SectionHeader
+        title={"Editor's Choice"}
+        subtitle={"Curated with love"}
+        seeMore={false}
+      />
+      <CardWrapper>
+        <Cards 
+          editor="annedean"
+          role="Associate Editor"
+          productImage="https://static.femaledaily.com/dyn/640/images/prod-pics/product_1558000129_YOU_MAKEUP_800x800.png"
+          productName="Y.O.U Makeups"
+          sectionType="editor's choice"
+          productRating="4.1"
+          productDescription="Rouge Velvet Matte Lip Cream"
+        />
+        <Cards 
+          editor="annedean"
+          role="Associate Editor"
+          productImage="https://static.femaledaily.com/dyn/640/images/prod-pics/product_1558000129_YOU_MAKEUP_800x800.png"
+          productName="Y.O.U Makeups"
+          sectionType="editor's choice"
+          productRating="4.1"
+          productDescription="Rouge Velvet Matte Lip Cream"
+        />
+        <Cards 
+          editor="annedean"
+          role="Associate Editor"
+          productImage="https://static.femaledaily.com/dyn/640/images/prod-pics/product_1558000129_YOU_MAKEUP_800x800.png"
+          productName="Y.O.U Makeups"
+          sectionType="editor's choice"
+          productRating="4.1"
+          productDescription="Rouge Velvet Matte Lip Cream"
+        />
+        <Cards 
+          editor="annedean"
+          role="Associate Editor"
+          productImage="https://static.femaledaily.com/dyn/640/images/prod-pics/product_1558000129_YOU_MAKEUP_800x800.png"
+          productName="Y.O.U Makeups"
+          sectionType="editor's choice"
+          productRating="4.1"
+          productDescription="Rouge Velvet Matte Lip Cream"
+        />
+        <Cards 
+          editor="annedean"
+          role="Associate Editor"
+          productImage="https://static.femaledaily.com/dyn/640/images/prod-pics/product_1558000129_YOU_MAKEUP_800x800.png"
+          productName="Y.O.U Makeups"
+          sectionType="editor's choice"
+          productRating="4.1"
+          productDescription="Rouge Velvet Matte Lip Cream"
+        />
+      </CardWrapper>
+    </Container>
+  );
 }
 
 export default EditorChoice;
